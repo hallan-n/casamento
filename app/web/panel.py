c6ba7f5e9ca1f468bfa6c6cf3cffa1e1b216ec0a
hallan-n <hallanneves01@gmail.com>
2025-07-02 17:34:11 -0300

    Enhance guest and gift management in panel: add CRUD functionalities for gifts; improve error handling and notifications; refactor guest input handling.

--- app/web/panel.py
+++ app/web/panel.py
@@ -22,6 +22,7 @@ async def index():
                 return response.json()
             else:
                 ui.notify(f"Erro: {response.json()['detail']}", color="red")
+                return []
 
     async def post_guest(name: str, phone: str, description: str, is_confirmed: bool):
         async with httpx.AsyncClient() as client:
@@ -74,33 +75,96 @@ async def index():
             else:
                 ui.notify(f"Erro: {response.json()['detail']}", color="red")
 
-    menu()
+    async def get_gift():
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                "http://localhost:8000/gift/",
+                headers={"token": token},
+            )
+            if response.status_code == 200:
+                return response.json()
+            else:
+                ui.notify(f"Erro: {response.json()['detail']}", color="red")
+                return []
 
+    async def post_gift(name: str, url: str, thumb: str, price: str):
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                "http://localhost:8000/gift/",
+                json={
+                    "name": name,
+                    "url": url,
+                    "thumb": thumb,
+                    "price": price,
+                },
+                headers={"token": token},
+            )
+            if response.status_code == 200:
+                ui.notify("Sucesso!", color="green")
+                await ui.run_javascript("window.location.reload();")
+            else:
+                ui.notify(f"Erro: {response.json()['detail']}", color="red")
+
+    async def put_gift(id: str, name: str, url: str, thumb: str, price: str):
+        async with httpx.AsyncClient() as client:
+            response = await client.put(
+                "http://localhost:8000/gift/",
+                json={
+                    "id": id,
+                    "name": name,
+                    "url": url,
+                    "thumb": thumb,
+                    "price": price,
+                },
+                headers={"token": token},
+            )
+            if response.status_code == 200:
+                ui.notify("Presente atualizado com sucesso!", color="green")
+                await ui.run_javascript("window.location.reload();")
+            else:
+                ui.notify(f"Erro: {response.json()['detail']}", color="red")
+
+    async def delete_gift(id: str):
+        async with httpx.AsyncClient() as client:
+            response = await client.delete(
+                f"http://localhost:8000/gift/?id={id}",
+                headers={"token": token},
+            )
+            if response.status_code == 200:
+                ui.notify("Presente excluido com sucesso!", color="green")
+                await ui.run_javascript("window.location.reload();")
+            else:
+                ui.notify(f"Erro: {response.json()['detail']}", color="red")
+
+    menu()
+    ui.add_head_html("""<style>*{overflow: auto;}</style>""")
     with ui.element("div").classes("w-full mt-10"):
         with ui.element("div").classes(
-            "flex flex-row max-w-[1200px] mx-auto w-full h-screen gap-6 justify-center"
+            "grid grid-cols-3 gap-6 max-w-[1200px] mx-auto w-full h-full"
         ):
-            with ui.element("div").classes("flex flex-col gap-4 w-full max-w-[400px]"):
+            with ui.element("div").classes(
+                "flex justify-between flex-col gap-4 w-full max-h-[400px]"
+            ):
                 ui.label("Adicionar convidado").classes("text-bold text-lg")
-                add_name = ui.input(label="Nome")
-                add_phone = ui.input(label="Telefone")
-                add_description = ui.input(label="Descrição")
-                add_is_confirmed = ui.checkbox("Confirmado", value=False)
+                guest_name = ui.input(label="Nome")
+                guest_phone = ui.input(label="Telefone")
+                guest_description = ui.input(label="Descrição")
+                guest_is_confirmed = ui.checkbox("Confirmado", value=False)
                 ui.button(
                     text="Adicionar",
                     icon="add",
                     on_click=lambda: post_guest(
-                        add_name.value,
-                        add_phone.value,
-                        add_description.value,
-                        add_is_confirmed.value,
+                        guest_name.value,
+                        guest_phone.value,
+                        guest_description.value,
+                        guest_is_confirmed.value,
                     ),
                     color=None,
                 ).classes("bg-[#6b6d4a] text-white w-full")
 
             guests = await get_guest()
 
-            with ui.element("div").classes("w-full h-full max-h-[500px] max-w-[720px]"):
+            with ui.element("div").classes("col-span-2 w-full h-full max-h-[400px]"):
                 ui.label("Lista de convidados").classes("text-bold text-lg")
                 with ui.column().classes("overflow-y-scroll h-full"):
                     for item in guests:
@@ -111,16 +175,16 @@ async def index():
                                 ui.input("ID").props("readonly").classes(
                                     "w-24"
                                 ).value = item["id"]
-                                name = ui.input("Nome", value=item["name"]).classes(
-                                    "w-24"
-                                )
-                                phone = ui.input(
+                                update_guest_name = ui.input(
+                                    "Nome", value=item["name"]
+                                ).classes("w-24")
+                                update_guest_phone = ui.input(
                                     "Telefone", value=item["phone"]
                                 ).classes("w-24")
-                                description = ui.input(
+                                update_guest_description = ui.input(
                                     "Descrição", value=item["description"]
                                 ).classes("w-24")
-                                is_confirmed = ui.checkbox(
+                                update_guest_is_confirmed = ui.checkbox(
                                     "Confirmado", value=item["is_confirmed"]
                                 )
 
@@ -133,10 +197,10 @@ async def index():
                                             i, n.value, p.value, d.value, c.value
                                         ),
                                         i=item["id"],
-                                        n=name,
-                                        p=phone,
-                                        d=description,
-                                        c=is_confirmed,
+                                        n=update_guest_name,
+                                        p=update_guest_phone,
+                                        d=update_guest_description,
+                                        c=update_guest_is_confirmed,
                                     )
                                 )
 
@@ -145,6 +209,79 @@ async def index():
                                 ).on_click(
                                     partial(
                                         lambda _=None, i=None: delete_guest(i),
+                                        i=item["id"],
+                                    )
+                                )
+
+            with ui.element("div").classes(
+                "flex justify-between flex-col gap-4 w-full max-h-[400px] mt-20"
+            ):
+                ui.label("Adicionar presente").classes("text-bold text-lg")
+                gift_name = ui.input(label="Título")
+                gift_url = ui.input(label="Link")
+                gift_thumb = ui.input(label="URL da imagem")
+                gift_price = ui.input(label="Preço")
+
+                ui.button(
+                    text="Adicionar",
+                    icon="add",
+                    on_click=lambda: post_gift(
+                        gift_name.value,
+                        gift_url.value,
+                        gift_thumb.value,
+                        gift_price.value,
+                    ),
+                    color=None,
+                ).classes("bg-[#6b6d4a] text-white w-full")
+
+            gifts = await get_gift()
+            with ui.element("div").classes(
+                "col-span-2 w-full h-full max-h-[400px] mt-20"
+            ):
+                ui.label("Lista de presentes").classes("text-bold text-lg")
+                with ui.column().classes("overflow-y-scroll h-full"):
+                    for item in gifts:
+                        with ui.row().classes(
+                            "items-center justify-between w-full border p-4 sm:min-w-[680px] min-h-32 flex-nowrap"
+                        ):
+                            with ui.label().classes("flex flex-nowrap gap-3"):
+                                ui.input("ID").props("readonly").classes(
+                                    "w-24"
+                                ).value = item["id"]
+                                update_gift_name = ui.input(
+                                    label="Título", value=item["name"]
+                                ).classes("w-24")
+                                update_gift_url = ui.input(
+                                    label="Link", value=item["url"]
+                                ).classes("w-24")
+                                update_gift_thumb = ui.input(
+                                    label="URL da imagem", value=item["thumb"]
+                                ).classes("w-24")
+                                update_gift_price = ui.input(
+                                    label="Preço", value=item["price"]
+                                ).classes("w-24")
+
+                            with ui.row().classes("flex flex-row flex-nowrap gap-2"):
+                                ui.button(icon="check", color=None).classes(
+                                    "bg-[#6b6d4a] text-white"
+                                ).on_click(
+                                    partial(
+                                        lambda _=None, i=None, n=None, u=None, t=None, p=None: put_gift(
+                                            i, n.value, u.value, t.value, p.value
+                                        ),
+                                        i=item["id"],
+                                        n=update_gift_name,
+                                        u=update_gift_url,
+                                        t=update_gift_thumb,
+                                        p=update_gift_price,
+                                    )
+                                )
+
+                                ui.button(icon="delete", color=None).classes(
+                                    "bg-[#6b6d4a] text-white"
+                                ).on_click(
+                                    partial(
+                                        lambda _=None, i=None: delete_gift(i),
                                         i=item["id"],
                                     )
                                 )
