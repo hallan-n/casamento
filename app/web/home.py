5c73242f741cef6097ae6bdbfbbc16b46e27f1b3
hallan-n <hallanneves01@gmail.com>
2025-07-15 09:36:17 -0300

    Enhance home page layout and update event details for clarity

--- app/web/home.py
+++ app/web/home.py
@@ -16,16 +16,18 @@ async def index():
             "flex justify-center lg:justify-end h-full max-w-[1200px] mx-auto"
         ):
             with ui.element("div").classes(
-                "flex flex-col items-center mt-28 md:mt-0 md:justify-center gap-2 text-white text-lg home-content"
+                "z-[1000] flex flex-col items-center mt-28 md:mt-0 md:justify-center gap-2 text-white text-lg home-content"
             ):
                 ui.image("app/web/assets/logo_full.svg")
                 ui.image("app/web/assets/divider.svg")
                 ui.label("Save the Date")
-                ui.label("Sábado, 04 de Outubro!")
+                ui.label("Sábado, 04 de Outubro, às 16h!").classes('text-bold')
                 ui.label("Gostaríamos de convidá-lo(a)")
                 ui.label("para celebrar conosco este")
                 ui.label("momento especial de união.")
-                with ui.element('div').classes('flex flex-nowrap items-center gap-4 rounded-lg w-full p-4').style("""background-color: rgba(0, 0, 0, 0.2);"""):
+                with ui.element('a').classes(
+                    'bg-black/20 hover:bg-black/50 flex flex-nowrap items-center gap-4 rounded-lg w-full p-4'
+                ).props('href=https://maps.app.goo.gl/VH5fz9o2oMAg3LMx9 target=_blank'):
                     ui.icon('location_on')
-                    ui.link("Paróquia de Santo André, Rua Santo André, 387", "https://maps.app.goo.gl/VH5fz9o2oMAg3LMx9", new_tab=True).classes("text-sm no-underline text-white")
+                    ui.label("Paróquia de Santo André, Rua Santo André, 387 | Às 16h").classes("text-sm no-underline text-white")
     ui.element("div").classes("home-sm")
